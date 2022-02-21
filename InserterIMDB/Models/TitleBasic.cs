326f2f202380bf6ae696fda1958bd6689aee9130
move <move@zealand.dk>
2022-02-21 09:16:29 +0100

    Add project files.

---
+++ InserterIMDB/Models/TitleBasic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InserterIMDB.Models
+{
+    class TitleBasic
+    {
+        public string tconst { get; set; }
+        public string titleType { get; set; }
+        public string primaryTitle { get; set; }
+        public string originalTitle { get; set; }
+        public bool isAdult { get; set; }
+        public int? startYear { get; set; }
+        public int? endYear { get; set; }
+        public int? runTimeMinutes { get; set; }
+    }
+}
---
+++ InserterIMDB/NormalInsert.cs
@@ -0,0 +1,54 @@
+﻿using InserterIMDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InserterIMDB
+{
+    class NormalInsert
+    {
+        public void InsertData(SqlConnection sqlConn, List<TitleBasic> allTitles)
+        {
+            //p4SSw0rd
+
+            foreach (TitleBasic myTitle in allTitles)
+            {
+                SqlCommand sqlComm = new SqlCommand("INSERT INTO [dbo].[TitlesBasic] " +
+           "([Tconst],[TitleType],[PrimaryTitle],[OriginalTitle] " +
+           ",[IsAdult],[StartYear],[EndYear],[RuntimeMinutes]) " +
+            "VALUES " +
+           "('" + myTitle.tconst + "'" +
+           ",'" + myTitle.titleType + "' " +
+          " ,'" + myTitle.primaryTitle + "' " +
+           ",'" + myTitle.originalTitle + "' " +
+           ", " + (myTitle.isAdult ? 1 : 0) +
+           "," + CheckForNull(myTitle.startYear) +
+           "," + CheckForNull(myTitle.endYear) +
+           "," + CheckForNull(myTitle.runTimeMinutes) + ")", sqlConn);
+
+                try
+                {
+                    sqlComm.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(sqlComm.CommandText);
+                    throw ex;
+                }
+
+            }
+
+        }
+        private string CheckForNull(int? input)
+        {
+            if (input == null)
+            {
+                return "NULL";
+            }
+            return "" + input;
+        }
+    }
+}
